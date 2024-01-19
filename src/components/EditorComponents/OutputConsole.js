b1315336b8fcb72794f115716404f9f7f99c78ad
SamarthKadam <samarthskadam14@gmail.com>
2024-01-19 21:29:19 +0530

    small design change

--- src/components/EditorComponents/OutputConsole.js
+++ src/components/EditorComponents/OutputConsole.js
@@ -1,6 +1,18 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
 
 export default function OutputConsole() {
+
+    const [isIncreasing, setIsIncreasing] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIsIncreasing((prev) => !prev);
+    }, 2500); // Change the interval duration as needed
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className='mt-5 mx-4  max-[634px]:mx-2 h-[100%] relative'>
         <div className="border-2 h-[85%] border-[#3434]">
@@ -13,12 +25,12 @@ export default function OutputConsole() {
         <div className='bg-[#FF747B] h-2 max-[992px]:h-1 w-[40%] rounded-md'></div>
         <div className='flex gap-1'>
         <div className='bg-[#75D2F0] h-2 max-[992px]:h-1 w-[40%] rounded-md'></div>
+        <div className={`bg-[#FFFFFF] h-2 max-[992px]:h-1 w-[20%] rounded-md transition-all duration-500 ${isIncreasing ? 'w-[10%]' : 'w-[20%]'}`}></div>
         <div className='bg-[#F1D65E] h-2 max-[992px]:h-1 w-[20%] rounded-md'></div>
-        <div className='bg-[#FFFFFF] h-2 max-[992px]:h-1 w-[20%] rounded-md'></div>
         </div>
         <div className='bg-[#9070F4] h-2 max-[992px]:h-1 w-[70%] rounded-md'></div>
         <div className='flex gap-1'>
-        <div className='bg-[#75D2F0] h-2 max-[992px]:h-1 w-[60%] rounded-md'></div>
+        <div className={`bg-[#75D2F0] h-2 max-[992px]:h-1 w-[40%] rounded-md transition-all duration-500 ${isIncreasing ? 'w-[40%]' : 'w-[50%]'} `}></div>
         <div className='bg-[#F1D65E] h-2 max-[992px]:h-1 w-[20%] rounded-md'></div>
         </div>
         <div className='bg-[#FFFFFF] h-2 max-[992px]:h-1 w-[40%] rounded-md'></div>
@@ -29,17 +41,17 @@ export default function OutputConsole() {
         </div>
         <div className='bg-[#F1D65E] h-2 max-[992px]:h-1 w-[36%] rounded-md'></div>
         <div className='flex gap-1'>
-        <div className='bg-[#FF747B] h-2 max-[992px]:h-1 w-[40%] rounded-md'></div>
+        <div className={`bg-[#FF747B] h-2 max-[992px]:h-1 w-[40%] rounded-md transition-all duration-500 ${isIncreasing ? 'w-[40%]' : 'w-[60%]'} `}></div>
         <div className='bg-[#FFFFFF] h-2 max-[992px]:h-1 w-[20%] rounded-md'></div>
         </div>
         <div className='flex gap-1'>
-        <div className='bg-[#75D2F0] h-2 max-[992px]:h-1 w-[15%] rounded-md'></div>
+        <div className={`bg-[#75D2F0] h-2 max-[992px]:h-1 w-[15%] rounded-md transition-all duration-500 ${isIncreasing ? 'w-[15%]' : 'w-[30%]'} `}></div>
         <div className='bg-[#9070F4] h-2 max-[992px]:h-1 w-[20%] rounded-md'></div>
         <div className='bg-[#F1D65E] h-2 max-[992px]:h-1 w-[30%] rounded-md'></div>
         </div>
         <div className='flex gap-1'>
         <div className='bg-[#75D2F0] h-2 max-[992px]:h-1 w-[40%] rounded-md'></div>
-        <div className='bg-[#FFFFFF] h-2 max-[992px]:h-1 w-[20%] rounded-md'></div>
+        <div className={`bg-[#FFFFFF] h-2 max-[992px]:h-1 w-[20%] rounded-md transition-all duration-500 ${isIncreasing ? 'w-[20%]' : 'w-[40%]'} `}></div>
         </div>
         </div>
         </div>
